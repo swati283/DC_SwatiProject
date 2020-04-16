523df5224181b0a9f7bbcdab6b576c43776f2fb6
Himanshu <Himanshu@DESKTOP-1OE6IMV>
2020-04-17 00:32:37 +0530

    I have done changes in Class1 file

--- SwatiProject/src/Package1/Class1.java
+++ SwatiProject/src/Package1/Class1.java
@@ -8,6 +8,8 @@ public class Class1 {
 	public void display1()
 	{
 		System.out.println("This is first program-Method1");
+		System.out.println("Hi, i have made the changes");
+		
 	}
 	public void display2()
 	{
